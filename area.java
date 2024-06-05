2085a4ddc21e69db75ccaecc20da4056d8387d51
192211838 <124235318+192211838@users.noreply.github.com>
2024-06-05 22:05:13 +0530

    Create area.java

---
+++ area.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Rect
+{
+	public static void main(String args[])
+{
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter the Length:");
+	int l = s.nextInt();
+	int a = l*l;
+	System.out.println("Area of the Rectangle:"+a);
+}
+}
